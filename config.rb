3b8a7c60885a1190ce16ba9a988d506548721dbc
Andrés González <andres@simbiotica.es>
2014-11-26 19:03:04 +0100

    index

--- config.rb
+++ config.rb
@@ -4,3 +4,13 @@ css_dir = "dist"
 images_dir = "img"
 relative_assets = true
 output_style = :compressed
+
+require 'autoprefixer-rails'
+require 'compass-flexbox'
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << AutoprefixerRails.process(css)
+  end
+end
